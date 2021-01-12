bec5ca5b9bff4c0a2e1a08caed49fd72ab337621
Austinbreathesair <austinbreathesair@gmail.com>
2021-01-12 11:47:17 +0200

    intial project scaffold

---
+++ src/Firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyBuC8q30u-qzGMBsaoVwnwWEkjYnvSLw24",
+  authDomain: "vue-chat-2077c.firebaseapp.com",
+  projectId: "vue-chat-2077c",
+  storageBucket: "vue-chat-2077c.appspot.com",
+  messagingSenderId: "758774114058",
+  appId: "1:758774114058:web:2e0c3b62a8cc31ca28fcf3"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
